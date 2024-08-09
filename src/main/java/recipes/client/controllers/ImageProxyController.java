9c5d2e18a9d831c372d128f238052df9bf0a58d9
Ruslan Orlov <org1978org@gmail.com>
2024-08-09 23:41:35 +0500

    Commit the changes of handling images #3 (2024-08-09)

---
+++ src/main/java/recipes/client/controllers/ImageProxyController.java
@@ -0,0 +1,40 @@
+package recipes.client.controllers;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.RequiredArgsConstructor;
+import recipes.client.services.SessionService;
+
+@Controller
+@RequiredArgsConstructor
+public class ImageProxyController {
+	
+	private final SessionService sessionService;
+	
+	private RestTemplate restTemplate = new RestTemplate();
+	
+	private String url = "http://localhost:8080/api/v1/recipes/{id}/image";
+	
+	@GetMapping("/recipes/{id}/image")
+	public ResponseEntity<byte[]> getRecipeImage(@PathVariable Long id) {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("Authorization", sessionService.getAuthHeader());
+		
+		ResponseEntity<byte[]> response = 
+				this.restTemplate.exchange(url, HttpMethod.GET, 
+						new HttpEntity<>(httpHeaders), byte[].class, id);
+		
+		if (response.getStatusCode().is2xxSuccessful()) {
+			return response;
+		}
+		return null;
+	}
+	
+}
